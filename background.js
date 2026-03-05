d4bcb52440a1b9e510bacf490957180823f369ba
KazIDK <discordcopier@gmail.com>
2026-03-05 12:24:32 +0300

    a

--- background.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("bgColorPicker").addEventListener("change", function() {
-    let color = this.value;
-    document.body.style.backgroundColor = color;
-    localStorage.setItem("bgColor", color);
-});
